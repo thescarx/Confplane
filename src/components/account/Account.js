21b706e70de8c285f02cc57c1a043063daf3ba12
ScarX0 <aa.djellouli@esi-sba.dz>
2022-06-19 03:43:03 +0100

    f

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -20,22 +20,21 @@ import Modal from "../Notification/Modal";
 import { notifications } from "../../data";
 import EditIcon from "@mui/icons-material/Edit";
 import Modifier from "../modifier/Modifer";
-import logo from "./ profile.png"
+import logo from "./ profile.png";
 
 function Account() {
   const [not, setNot] = useState(notifications);
   const socket = useContext(SocketContext);
 
-  // socket.onmessage=function(e){
-  //   const obj = JSON.parse(e["data"]);
-  //   for (let i = 0; i < obj.notifications.length; i++) {
-  //     notifications.push(obj.notifications[i]);
-  //     console.log(notifications);
-  //   }
-  //   setNot(notifications);
-  //   console.log("done");
-
-  // }
+  socket.onmessage = function (e) {
+    const obj = JSON.parse(e["data"]);
+    for (let i = 0; i < obj.notifications.length; i++) {
+      notifications.push(obj.notifications[i]);
+      console.log(notifications);
+    }
+    setNot(notifications);
+    console.log("done");
+  };
 
   // console.log("this is the data " + data);
   let host = "http://127.0.0.1:8000";
@@ -112,30 +111,30 @@ function Account() {
     const socket = new WebSocket(
       "ws://127.0.0.1:8000/ws/socket-server/?token=" + token
     );
-    socket.onmessage = function (e) {
-      const obj = JSON.parse(e["data"]);
-      console.log(obj.notifications.length);
+    // socket.onmessage = function (e) {
+    //   const obj = JSON.parse(e["data"]);
+    //   console.log(obj.notifications.length);
 
-      for (let i = 0; i < obj.notifications.length; i++) {
-        notifications.push(obj.notifications[i]);
-      }
-      // console.log("all notifications");
-      // for(let i = 0;i<obj.notifications.length-1;i++){
-      //   for(let y = 0; y<obj.notifications)
-      // }
-      if (notifications.length > 1) {
-        for (let i = 0; i < notifications.length; i++) {
-          for (let x = 0; x < obj.notifications.length; x++) {
-            if (notifications[i].id === obj.notifications[x].id) {
-              notifications.pop(obj.notifications[x]);
-            }
-          }
-        }
-      }
-      setNot(obj.notifications);
+    //   for (let i = 0; i < obj.notifications.length; i++) {
+    //     notifications.push(obj.notifications[i]);
+    //   }
+    //   // console.log("all notifications");
+    //   // for(let i = 0;i<obj.notifications.length-1;i++){
+    //   //   for(let y = 0; y<obj.notifications)
+    //   // }
+    //   // if (notifications.length > 1) {
+    //   //   for (let i = 0; i < notifications.length; i++) {
+    //   //     for (let x = 0; x < obj.notifications.length; x++) {
+    //   //       if (notifications[i].id === obj.notifications[x].id) {
+    //   //         notifications.pop(obj.notifications[x]);
+    //   //       }
+    //   //     }
+    //   //   }
+    //   // }
+    //   setNot(obj.notifications);
 
-      console.log(not);
-    };
+    //   console.log(not);
+    // };
     axios
       .get(host + "/articles/list/path")
       .then((artc) => {
@@ -382,13 +381,16 @@ function Account() {
                         name="file_up"
                       />
                     </div>
-                    {data_profile.profile_picture===null &&<img src={logo} alt={logo}/>}
-                    {data_profile.profile_picture!== null &&
-                    <img
-                      ref={hiddenFileInput}
-                      src={host + data_profile.profile_picture}
-                      alt={host + data_profile.profile_picture}
-                    />}
+                    {data_profile.profile_picture === null && (
+                      <img src={logo} alt={logo} />
+                    )}
+                    {data_profile.profile_picture !== null && (
+                      <img
+                        ref={hiddenFileInput}
+                        src={host + data_profile.profile_picture}
+                        alt={host + data_profile.profile_picture}
+                      />
+                    )}
                   </div>
 
                   {/* //////////////////////////////////////////////////// */}
@@ -430,6 +432,7 @@ function Account() {
                           onClick={() => {
                             localStorage.clear();
                             navigate_6("/login");
+                            window.location.reload();
                           }}
                         >
                           Logout
